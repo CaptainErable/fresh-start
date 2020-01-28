e23ae23e59085a16a8b3cb380f7fcdda77e09edd
CaptainErable <60198767+CaptainErable@users.noreply.github.com>
2020-01-28 15:11:57 -0500

    Updated descriptions in file for better clarity

--- 3B.R
+++ 3B.R
@@ -1,6 +1,9 @@
-A <- c(-1,+1,-1,+1)
-B <- c(-1,-1,+1,+1)
-y <- c(52,74,62,80)
+#Part of Module 3 of Experimentation for Improvement on Coursera
+#Case study looking at developping a linear model for number of popped corn with some initial data points
+
+A <- c(-1,+1,-1,+1)                     # -1 : 160s, +1 : 200s
+B <- c(-1,-1,+1,+1)                     # -1 : white corn, +1 : yellow corn
+y <- c(52,74,62,80)                     # number of kernels popped
 popped_corn = lm(y ~ A + B + A*B)
 summary(popped_corn)
 
